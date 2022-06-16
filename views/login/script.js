72240b47b4fdd62f0fe2204b9649ac8b1dbf6d76
Abhishek <dekaa331@gmail.com>
2022-06-16 23:40:25 +0530

    login screen added

--- views/login/script.js
+++ views/login/script.js
@@ -1,21 +1,25 @@
-function login(e) {
+function signup(e) {
     e.preventDefault();
- 
+
+    const name = document.getElementById('name');
     const email = document.getElementById('email');
+    const tel = document.getElementById('tel');
     const pass = document.getElementById('pass');
 
     const obj = {
+        name: name.value,
         email: email.value,
+        tel: tel.value,
         pass: pass.value
     }
 
-    axios.post('http://localhost:3000/login', obj)
+    axios.post('http://localhost:3000/sign-up', obj)
         .then(response => {
             console.log(response);
-            // alert('Successfuly logged in');
+            alert('Successfuly signed up');
         })
         .catch(response => {
             console.log(response);
-            // alert('User already exists, Please Login');
+            alert('User already exists, Please Login');
         });
 }
