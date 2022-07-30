fdd9fd085c184129e121f7827412badc623b2b4a
Abhishek-675 <94445399+Abhishek-675@users.noreply.github.com>
2022-07-30 18:50:45 +0530

    Update S3services.js

--- services/S3services.js
+++ services/S3services.js
@@ -1,17 +1,14 @@
 const AWS = require('aws-sdk');
 
 exports.uploadToS3 = (data, filename) => {
-    const BUCKET_NAME = 'expense-tracker-2';
-    const IAM_USER_KEY = 'AKIAV3POSGEYU42KRT57';
-    const IAM_USER_SECRET = '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';
-
+    
     let s3bucket = new AWS.S3({
-        accessKeyId: IAM_USER_KEY,
-        secretAccessKey: IAM_USER_SECRET,
+        accessKeyId: process.env.IAM_USER_KEY,
+        secretAccessKey: process.env.IAM_USER_SECRET,
     })
 
     var params = {
-        Bucket: BUCKET_NAME,
+        Bucket: process.env.BUCKET_NAME,
         Key: filename,
         Body: data,
         ACL: 'public-read'
